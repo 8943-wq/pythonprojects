9ef3cd091d006337a5b6a5322f6742dc1e61ab9b
muhamed sarıkaya <83880998+8943-wq@users.noreply.github.com>
2023-02-22 08:35:32 +0300

    Update örnek .py

--- örnek .py
+++ örnek .py
@@ -1,16 +1,4 @@
-#ÖRNEK-1 DERS NOTU 
 
-#print("MERHABA İTÜLÜ ")
-#vize = input("vize notunuzu giriniz : ")
-#final = input("final notunu giriniz : ") 
-#ortalama = int(vize)*0.4 + int(final)*0.6 
-#if ortalama >=50  :
- #   harf = "   GEÇTİNİZ"
-#elif ortalama<50 : 
-#    harf ="   DAHA ÇOK ÇALIŞMANIZ GEREK..."     
-#print("DÖNEM SONU ORTALAMANIZ : {0}".format(ortalama)+harf) 
-
-#ÖRNEK-2 MAAŞIN ZAM ORANINA GÖRE BELİRLENMESİ 
 
 print("YENİ YIL ENFLASYON ORANINA GÖRE DÜZENLENEN MAAŞ ÖĞRENME SAYFASINA HOŞGELDİNİZ")
 
